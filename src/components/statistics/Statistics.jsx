b46c283e0c43431c3a4d8c71610c5848fba58201
vitalij99 <vitalisklymko@gmail.com>
2023-03-05 16:29:33 +0200

    add if to title

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,13 +2,16 @@ import styles from './statistics.module.css'
 import PropTypes from 'prop-types' 
 
 
-const Statistics = ({title,stats}) => {
+const Statistics = ({ title, stats }) => {
+    
+     
     return (
-        <section className={styles.statistics }>
-            <h2 className={styles.title}>{title}</h2>
-        <ul className={styles.list}>
-            { stats.map(Item) }                
-        </ul>
+        <section className={styles.statistics}>
+            { title ? <h2 className={styles.title}>{title}</h2> : "" }
+            
+            <ul className={styles.list}>
+                { stats.map(Item) }                
+            </ul>
         </section>
     )
 }
